0d28741f0df24c7b20b7ff60a0695bfbba784065
Pau Sentís <pau.sf@coditramuntana.com>
2022-09-04 21:03:47 +0200

    [FEAUTRE] Services

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
   def landing
-    
+    @services = %w(rehabilitacions obra_nova reformes enderrocs obra_publica 
+      manteniments impermeabilitzacions aillaments departament_tecnic)
   end
 end
